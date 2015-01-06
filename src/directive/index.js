238038ef57bea5c9425ab243ea25657b912e14fe
tsaikd <tsaikd@gmail.com>
2015-01-06 15:31:35 +0800

    refine excel parser
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,8 +1,8 @@
 app
 
 .controller("indexCtrl"
-	, [       "$scope", "$state", "$mdToast", "$filter", "Redmine", "Loading", "localStorageService"
-	, function($scope,   $state,   $mdToast,   $filter,   Redmine,   Loading,   localStorageService) {
+	, [       "$scope", "$state", "$mdToast", "$filter", "Redmine", "Loading", "localStorageService", "Excel"
+	, function($scope,   $state,   $mdToast,   $filter,   Redmine,   Loading,   localStorageService,   Excel) {
 
 	$scope.excelFile = [];
 	$scope.wbmap = [];
@@ -16,242 +16,14 @@ app
 		$scope.parseExcel();
 	});
 
-	function x_axis_to_num(x) {
-		x = x.toUpperCase();
-		var num = 0;
-		for (var i=0 ; i<x.length ; i++) {
-			var code = x[i].charCodeAt() - "A".charCodeAt() + 1;
-			num = num * 26 + code;
-		}
-		return num;
-	}
-
-	function num_to_x_axis(num) {
-		var x = "";
-		var xnum;
-		var code;
-		while (num > 0) {
-			xnum = (num-1) % 26;
-			num = parseInt((num-1) / 26);
-			code = "A".charCodeAt() + xnum;
-			x = String.fromCharCode(code) + x;
-		}
-		return x;
-	}
-
-	function next_x_axis(x) {
-		var num = x_axis_to_num(x);
-		return num_to_x_axis(num + 1);
-	}
-
-	function gen_enum_getter(enumArray) {
-		return function(value) {
-			if (!value || !enumArray || !enumArray.length) {
-				return -1;
-			}
-			for (var i=0 ; i<enumArray.length ; i++) {
-				var enumInfo = enumArray[i];
-				if (value == enumInfo.name) {
-					return +enumInfo.id;
-				}
-			}
-			return -1;
-		};
-	}
-
-	// dynamic generate for i18N
-	function gen_valid_wb_header_map() {
-		var valid_wb_header_map = {};
-		["subject"].forEach(function(key) {
-			valid_wb_header_map[$filter("translate")(key)] = valid_wb_header_map[key] = {
-				key: key,
-				nullable: false,
-				filter: function(value) {
-					value = "" + value;
-					value = value.trim();
-					return value ? value : undefined;
-				}
-			};
-		});
-		["description"].forEach(function(key) {
-			valid_wb_header_map[$filter("translate")(key)] = valid_wb_header_map[key] = {
-				key: key,
-				nullable: true,
-				filter: function(value) {
-					value = "" + value;
-					value = value.trim();
-					return value;
-				}
-			};
-		});
-		var enumInfos = [
-			{
-				key: "tracker_id",
-				nullable: false,
-				data: $scope.trackerData["trackers"]
-			},
-			{
-				key: "status_id",
-				nullable: false,
-				data: $scope.issueStatusData["issue_statuses"]
-			},
-			{
-				key: "priority_id",
-				nullable: false,
-				data: $scope.priorityData["issue_priorities"]
-			}
-		];
-		enumInfos.forEach(function(enumInfo) {
-			var getter = gen_enum_getter(enumInfo.data);
-			valid_wb_header_map[$filter("translate")(enumInfo.key)] = valid_wb_header_map[enumInfo.key] = {
-				key: enumInfo.key,
-				filter: function(value) {
-					var id = getter(value);
-					return id >= 0 ? id : undefined;
-				}
-			};
-		});
-		["start_date", "due_date"].forEach(function(key) {
-			valid_wb_header_map[$filter("translate")(key)] = valid_wb_header_map[key] = {
-				key: key,
-				nullable: true,
-				filter: function(value) {
-					var ma;
-					var strdate;
-					var date;
-					value = "" + value;
-					value = value.trim();
-					if (ma = value.match(/(\d{4})[-/](\d{1,2})[-/](\d{1,2})/)) {
-						strdate = ma[1] + "-" + ma[2] + "-" + ma[3];
-					} else if (ma = value.match(/(\d{1,2})\/(\d{1,2})\/(\d{2})/)) {
-						strdate = "20" + ma[3] + "-" + ma[1] + "-" + ma[2];
-					}
-					if (strdate) {
-						date = new Date(strdate);
-						if (date) {
-							return $filter("date")(date, "yyyy-MM-dd");
-						}
-					}
-					return undefined;
-				}
-			};
-		});
-		if ($scope.customFieldsData && $scope.customFieldsData["custom_fields"]) {
-			$scope.customFieldsData["custom_fields"].forEach(function(field) {
-				valid_wb_header_map[field.name] = {
-					key: "custom_fields",
-					nullable: true,
-					field: field,
-					filter: function(value) {
-						return value;
-					}
-				};
-			});
-		}
-		return valid_wb_header_map;
-	}
-
-	function to_wbmap(workbook) {
-		var result = [];
-		var valid_wb_header_map = gen_valid_wb_header_map();
-		workbook.SheetNames.forEach(function(sheetName) {
-			var formulae = XLSX.utils.get_formulae(workbook.Sheets[sheetName]);
-			if (!formulae) {
-				return;
-			}
-
-			var sheet = {
-				name: sheetName,
-				data: {},
-				headers: [],
-				xinfo: {},
-				yinfo: {},
-				stat: {
-					errorcount: 0
-				},
-				xaxis: [],
-				yaxis: []
-			};
-
-			var ref = workbook.Sheets[sheetName]["!ref"].split(":")[1];
-			if (!ref) {
-				return;
-			}
-			var refx = ref.match(/[a-z]+/i); refx = refx && refx[0];
-			var refy = ref.match(/[0-9]+/); refy = refy && +refy[0];
-			for (var x="A" ; ; x=next_x_axis(x)) {
-				sheet.xaxis.push(x);
-				sheet.data[x] = {};
-				sheet.xinfo[x] = {};
-				if (x == refx) {
-					break;
-				}
-			}
-			for (var i=1 ; i<=refy ; i++) {
-				sheet.yaxis.push(i);
-				sheet.yinfo[i] = {};
-			}
-
-			formulae.forEach(function(cellraw) {
-				var cellsplit = cellraw.split("='");
-				var xy = cellsplit.shift();
-				var x = xy.match(/[a-z]+/i); x = x && x[0];
-				var y = xy.match(/[0-9]+/); y = y && +y[0];
-				var text = cellsplit.join("='");
-				sheet.data[x][y] = {
-					value: text
-				};
-			});
-
-			// parse valid header row
-			sheet.xaxis.forEach(function(x) {
-				if (!sheet.data[x][1]) {
-					return;
-				}
-				var headerInfo = valid_wb_header_map[sheet.data[x][1].value];
-				if (headerInfo) {
-					sheet.headers.push({
-						key: headerInfo.key,
-						x: x,
-						info: headerInfo
-					});
-					sheet.xinfo[x].exist = true;
-
-					sheet.yaxis.forEach(function(y) {
-						var data = sheet.data[x][y] = sheet.data[x][y] || {};
-						data.filtered = headerInfo.filter(data.value);
-						if (data.filtered) {
-							data.valid = true;
-						} else if (headerInfo.nullable) {
-							data.valid = true;
-						} else {
-							data.valid = false;
-						}
-						if (data.valid) {
-							sheet.stat.errorcount++;
-						}
-					});
-				}
-			});
-
-			result.push(sheet);
-		});
-		return result;
-	}
-
 	$scope.parseExcel = function() {
 		var file = $scope.excelFile[0];
 		if (!file) {
 			return;
 		}
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			var data = e.target.result;
-			var wb = XLSX.read(data, {type: "binary"});
-			$scope.wbmap = to_wbmap(wb);
-			$scope.$digest();
-		};
-		reader.readAsBinaryString(file);
+		Excel.parseFile(file).then(function(sheets) {
+			$scope.wbmap = sheets;
+		});
 	};
 
 	$scope.errMsg = function(data) {
@@ -302,49 +74,6 @@ app
 	});
 	$scope.reloadProjects();
 
-	$scope.trackerData = {};
-	$scope.reloadTrackers = function() {
-		Redmine.trackers().then(function(data) {
-			$scope.trackerData = data.data;
-			$scope.trackerData.selectIdx = "0";
-		}, function(data) {
-			$scope.errMsg(data);
-		});
-	};
-	$scope.reloadTrackers();
-
-	$scope.issueStatusData = {};
-	$scope.reloadIssueStatus = function() {
-		Redmine.Issue.status().then(function(data) {
-			$scope.issueStatusData = data.data;
-			$scope.issueStatusData.selectIdx = "0";
-		}, function(data) {
-			$scope.errMsg(data);
-		});
-	};
-	$scope.reloadIssueStatus();
-
-	$scope.priorityData = {};
-	$scope.reloadPriority = function() {
-		Redmine.priorities().then(function(data) {
-			$scope.priorityData = data.data;
-			$scope.priorityData.selectIdx = "0";
-		}, function(data) {
-			$scope.errMsg(data);
-		});
-	};
-	$scope.reloadPriority();
-
-	$scope.customFieldsData = {};
-	$scope.reloadCustomFields = function() {
-		Redmine.customFields().then(function(data) {
-			$scope.customFieldsData = data.data;
-		}, function(data) {
-			$scope.errMsg(data);
-		});
-	};
-	$scope.reloadCustomFields();
-
 	function is_issue_valid(issue) {
 		if (!issue) {
 			return false;
@@ -373,7 +102,7 @@ app
 					if (header.key == "custom_fields") {
 						issue["custom_fields"] = issue["custom_fields"] || [];
 						issue["custom_fields"].push({
-							id: header.info.field.id,
+							id: header.info.id,
 							value: sheet.data[x][y].filtered
 						});
 					} else {
---
+++ src/service/excel.js
@@ -0,0 +1,344 @@
+app
+
+.factory("Excel"
+	, [       "$q", "$filter", "Redmine"
+	, function($q,   $filter,   Redmine) {
+
+	function Excel() {}
+
+	var cache = {};
+
+	function loadRedmine() {
+		var deferred = $q.defer();
+
+		if (cache["loadRedmine"]) {
+			deferred.resolve(cache["loadRedmine"]);
+			return deferred.promise;
+		}
+
+		$q.all([
+			Redmine.trackers(),
+			Redmine.Issue.status(),
+			Redmine.priorities(),
+			Redmine.customFields()
+		]).then(function(data) {
+			cache["loadRedmine"] = {
+				trackers: data[0].data.trackers,
+				issue_statuses: data[1].data.issue_statuses,
+				issue_priorities: data[2].data.issue_priorities,
+				custom_fields: data[3].data.custom_fields
+			};
+			deferred.resolve(cache["loadRedmine"]);
+		}, function(data) {
+			deferred.reject(data);
+		});
+
+		return deferred.promise;
+	};
+	// cache redmine request
+	loadRedmine();
+
+	function genFieldInfos() {
+		var deferred = $q.defer();
+
+		loadRedmine().then(function(data) {
+			var fieldInfos = [
+				{
+					key: "subject",
+					names: ["Subject", "主旨"],
+					field_format: "string",
+					nullable: false
+				},
+				{
+					key: "description",
+					names: ["Description", "概述"],
+					field_format: "string",
+					nullable: true
+				},
+				{
+					key: "tracker_id",
+					names: ["Tracker", "追蹤標籤"],
+					field_format: "enum",
+					nullable: false,
+					possible_values: data["trackers"]
+				},
+				{
+					key: "status_id",
+					names: ["Status", "狀態"],
+					field_format: "enum",
+					nullable: false,
+					possible_values: data["issue_statuses"]
+				},
+				{
+					key: "priority_id",
+					names: ["Priority", "優先權"],
+					field_format: "enum",
+					nullable: false,
+					possible_values: data["issue_priorities"]
+				},
+				{
+					key: "start_date",
+					names: ["Start Date", "開始日期"],
+					field_format: "date",
+					nullable: true
+				},
+				{
+					key: "due_date",
+					names: ["Due Date", "完成日期"],
+					field_format: "date",
+					nullable: true
+				}
+			];
+			data["custom_fields"].forEach(function(field) {
+				fieldInfos.push(angular.extend({}, field, {
+					key: "custom_fields",
+					names: [field.name],
+					nullable: true
+				}));
+			});
+			deferred.resolve(fieldInfos);
+		});
+
+		return deferred.promise;
+	}
+
+	function genFieldMap() {
+		var deferred = $q.defer();
+		var fieldMap = {};
+
+		var field_filter_map = {
+			"string": function(field, value) {
+				value = "" + value;
+				value = value.trim();
+				return value ? value : undefined;
+			},
+			"bool": function(field, value) {
+				value = "" + value;
+				value = value.trim();
+				switch (value) {
+				case "1":
+				case "true":
+				case "checked":
+					return true;
+				case "0":
+				case "false":
+				case "":
+					return false;
+				}
+				return undefined;
+			},
+			"int": function(field, value) {
+				value = +value;
+				return value === NaN ? undefined : value;
+			},
+			"list": function(field, value) {
+				return value;
+				/* check in server side
+				for (var i=0 ; i<field.possible_values.length ; i++) {
+					if (value === field.possible_values[i].value) {
+						return value;
+					}
+				}
+				return undefined;
+				*/
+			},
+			"enum": function(field, value) {
+				var enumArray = field.possible_values;
+				if (!value || !enumArray || !enumArray.length) {
+					return undefined;
+				}
+				for (var i=0 ; i<enumArray.length ; i++) {
+					var enumInfo = enumArray[i];
+					if (value == enumInfo.name) {
+						return +enumInfo.id;
+					}
+				}
+				return undefined;
+			},
+			"date": function(field, value) {
+				var ma;
+				var strdate;
+				var date;
+				value = "" + value;
+				value = value.trim();
+				if (ma = value.match(/(\d{4})[-/](\d{1,2})[-/](\d{1,2})/)) {
+					strdate = ma[1] + "-" + ma[2] + "-" + ma[3];
+				} else if (ma = value.match(/(\d{1,2})\/(\d{1,2})\/(\d{2})/)) {
+					strdate = "20" + ma[3] + "-" + ma[1] + "-" + ma[2];
+				}
+				if (strdate) {
+					date = new Date(strdate);
+					if (date) {
+						return $filter("date")(date, "yyyy-MM-dd");
+					}
+				}
+				return undefined;
+			}
+		};
+
+		genFieldInfos().then(function(fieldInfos){
+			fieldInfos.forEach(function(fieldInfo) {
+				fieldInfo.filter = field_filter_map[fieldInfo["field_format"]];
+				if (!fieldInfo.filter) {
+					console.error("unknown field format: " + fieldInfo["field_format"], fieldInfo);
+				}
+				fieldInfo.names.forEach(function(name) {
+					fieldMap[name] = fieldInfo;
+				});
+			});
+			deferred.resolve(fieldMap);
+		});
+
+		return deferred.promise;
+	}
+
+	function x_axis_to_num(x) {
+		x = x.toUpperCase();
+		var num = 0;
+		for (var i=0 ; i<x.length ; i++) {
+			var code = x[i].charCodeAt() - "A".charCodeAt() + 1;
+			num = num * 26 + code;
+		}
+		return num;
+	}
+
+	function num_to_x_axis(num) {
+		var x = "";
+		var xnum;
+		var code;
+		while (num > 0) {
+			xnum = (num-1) % 26;
+			num = parseInt((num-1) / 26);
+			code = "A".charCodeAt() + xnum;
+			x = String.fromCharCode(code) + x;
+		}
+		return x;
+	}
+
+	function next_x_axis(x) {
+		var num = x_axis_to_num(x);
+		return num_to_x_axis(num + 1);
+	}
+
+	function handleWorkbookSheet(wbSheet, sheetName, fieldMap) {
+		var formulae = XLSX.utils.get_formulae(wbSheet);
+		if (!formulae) {
+			return;
+		}
+
+		var sheet = {
+			name: sheetName,
+			data: {},
+			headers: [],
+			xinfo: {},
+			yinfo: {},
+			stat: {
+				errorcount: 0
+			},
+			xaxis: [],
+			yaxis: []
+		};
+
+		var ref = wbSheet["!ref"].split(":")[1];
+		if (!ref) {
+			return;
+		}
+
+		var refx = ref.match(/[a-z]+/i); refx = refx && refx[0];
+		var refy = ref.match(/[0-9]+/); refy = refy && +refy[0];
+		for (var x="A" ; ; x=next_x_axis(x)) {
+			sheet.xaxis.push(x);
+			sheet.data[x] = {};
+			sheet.xinfo[x] = {};
+			if (x == refx) {
+				break;
+			}
+		}
+		for (var i=1 ; i<=refy ; i++) {
+			sheet.yaxis.push(i);
+			sheet.yinfo[i] = {};
+		}
+
+		formulae.forEach(function(cellraw) {
+			var cellsplit = cellraw.split("='");
+			var xy = cellsplit.shift();
+			var x = xy.match(/[a-z]+/i); x = x && x[0];
+			var y = xy.match(/[0-9]+/); y = y && +y[0];
+			var text = cellsplit.join("='");
+			sheet.data[x][y] = {
+				value: text
+			};
+		});
+
+		// parse valid header row
+		sheet.xaxis.forEach(function(x) {
+			if (!sheet.data[x][1]) {
+				return;
+			}
+			var headerInfo = fieldMap[sheet.data[x][1].value];
+			if (!headerInfo) {
+				return;
+			}
+			sheet.headers.push({
+				key: headerInfo.key,
+				x: x,
+				info: headerInfo
+			});
+			sheet.xinfo[x].exist = true;
+
+			sheet.yaxis.forEach(function(y) {
+				var data = sheet.data[x][y] = sheet.data[x][y] || {};
+				data.filtered = headerInfo.filter(headerInfo, data.value);
+				if (data.filtered) {
+					data.valid = true;
+				} else if (headerInfo.nullable) {
+					data.valid = true;
+				} else {
+					data.valid = false;
+				}
+				if (data.valid) {
+					sheet.stat.errorcount++;
+				}
+			});
+		});
+
+		return sheet;
+	}
+
+	function handleWorkbook(workbook) {
+		var deferred = $q.defer();
+
+		genFieldMap().then(function(fieldMap) {
+			var sheets = [];
+			workbook.SheetNames.forEach(function(sheetName) {
+				var sheet = handleWorkbookSheet(workbook.Sheets[sheetName], sheetName, fieldMap);
+				if (sheet) {
+					sheets.push(sheet);
+				}
+			});
+			deferred.resolve(sheets);
+		});
+
+		return deferred.promise;
+	}
+
+	Excel.prototype.parseFile = function(file) {
+		var deferred = $q.defer();
+
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			var data = e.target.result;
+			var wb = XLSX.read(data, {type: "binary"});
+			handleWorkbook(wb).then(function(data) {
+				deferred.resolve(data);
+			});
+		};
+		reader.readAsBinaryString(file);
+
+		return deferred.promise;
+	};
+
+	return new Excel();
+}])
+
+;
